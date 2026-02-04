22d9f4224fd34259e6763916d8de3c23dfd9ea26
luatnc <luatnc@efy.com.vn>
2026-02-04 10:30:05 +0700

    fix: menu mobile

--- resources/views/client/layouts/menu.blade.php
+++ resources/views/client/layouts/menu.blade.php
@@ -195,15 +195,154 @@
         font-size: 12px;
         background: #fff;
     }
+    /* css ẩn hiện lọc */
+    .filter-box {
+        display: none;          /* ẨN BAN ĐẦU */
+        margin-top: 8px;
+    }
 
+    .filter-box select {
+        width: 95%;
+        padding: 6px 8px;
+        margin: 0px 10px 2px 10px;
+        border: 1px solid #000;
+        background: #fff;
+    }
+
+    .toggle-btn {
+        background: none;
+        border: 1px solid #000;
+        width: 24px;
+        height: 24px;
+        cursor: pointer;
+        font-size: 16px;
+        line-height: 1;
+    }
+
+    .filter-header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between; /* đẩy + sang phải */
+        width: 95%;                    /* KÉO DÀI */
+        padding: 10px 12px;
+        border: 1px solid #000;
+        box-sizing: border-box;
+        margin: auto;
+    }
+
+    .filter-text {
+        font-size: 12px;
+        letter-spacing: 1px;
+    }
+
+    .toggle-btn {
+        width: 24px;
+        height: 24px;
+        border: 1px solid #000;
+        background: #fff;
+        cursor: pointer;
+        font-size: 16px;
+        line-height: 1;
+    }
+    .mobile{
+        display: none;
+    }
+
+
+    /* ===============================
+   RESPONSIVE HEADER – KITE
+   =============================== */
+
+    /* -------- Tablet & Mobile -------- */
+    @media (max-width: 991px) {
+
+        .mobile{
+            display: none !important;
+        }
+
+        .nav-inner {
+            flex-direction: column;
+            align-items: flex-start;
+        }
+
+        .nav-right {
+            flex-direction: column;
+            gap: 20px;
+            width: 100%;
+        }
+
+        /* LOGO */
+        .nav-logo {
+            width: 100%;
+        }
+
+        .nav-logo .logo-link {
+            font-size: 36px;
+            letter-spacing: 10px;
+        }
+
+        /* MENU */
+        .nav-menu {
+            padding-left: 0;
+        }
+
+        .nav-menu li {
+            margin-bottom: 6px;
+        }
+
+        .nav-menu a {
+            font-size: 13px;
+        }
+
+        /* FILTER */
+        .nav-filter {
+            width: 100%;
+            max-width: 260px;
+        }
+
+        .filter-title {
+            text-align: left;
+        }
+    }
+
+    /* -------- Mobile nhỏ -------- */
+    @media (max-width: 768px) {
+
+        .mobile{
+            display: block !important;
+        }
+        .nav-logo .logo-link {
+            font-size: 35px;
+            letter-spacing: 8px;
+        }
+
+        .nav-menu a {
+            font-size: 12px;
+        }
+
+        .nav-filter {
+            max-width: 100%;
+        }
+
+        .nav-filter select {
+            font-size: 11px;
+        }
+
+        .web {
+            display: none !important;
+        }
+        /* .nav-logo-mobile{
+            width: 80%%;
+        } */
+    }
 </style>
 <nav id="main_nav" class="navbar navbar-expand-lg navbar-light bg-white" style="top:0;padding-top:0px !important;padding-bottom: 0px !important;background:#ffffff!important;width: 100%;z-index: 1000;">
     <div class="container d-flex justify-content-between align-items-center">
         <div class="nav-inner w-100 d-flex justify-content-between align-items-start">
-            <!-- RIGHT: MENU + SEARCH -->
-            <div class="nav-right d-flex">
+            <!-- PC --> 
+            <div class="nav-right d-flex web">
                 <div class="nav-logo">
-                    <a href="/" class="logo-link">KITE</a>
+                    <a href="/" class="logo-link">ZICZAC</a>
                 </div>
                 <!-- MENU -->
                 <ul class="nav-menu list-unstyled mb-0">
@@ -228,9 +367,44 @@
 
             </div>
         </div>
-
     </div>
 </nav>
+
+<!-- MOBILE -->
+ <div class="mobile">
+
+    <div class="d-flex">
+        <div style="width:20%;color:#6f6969;margin-left: 8px;">
+            <span>en</span>
+        </div>
+        <div class="nav-logo" style="width:60%">
+            <a href="/" class="logo-link">ZICZAC</a> <br>
+            <span style="font-size: 13px;font-family: monospace;">ARCHITECTURE</span>
+        </div>
+        <div >
+            <a href="/client/home/about" style="font-size: 12px;text-decoration: none;color:#6f6969">Giới thiệu</a>
+        </div>
+    </div>
+    <div style="width:95%;height:1px;background: #545454;margin: 0 auto;margin:10px"></div>
+    <div class="filter-header">
+        <span class="filter-text">FILTER</span>
+        <button id="toggleFilter" class="toggle-btn">+</button>
+    </div>
+
+    <div id="filterBox" class="filter-box">
+        <select>
+            <option>typology</option>
+        </select>
+        <select>
+            <option>projects</option>
+        </select>
+        <select>
+            <option>year</option>
+        </select>
+    </div>
+
+ </div>
+
 <!-- Close Header -->
 @section('js')
 <script>
@@ -252,5 +426,17 @@
             $("#navbar-toggler").removeClass('show');
         }
     }
+
+
+    // ẩn hiện lọc 
+    const btn = document.getElementById("toggleFilter");
+    const box = document.getElementById("filterBox");
+
+    btn.addEventListener("click", () => {
+        const isOpen = box.style.display === "block";
+
+        box.style.display = isOpen ? "none" : "block";
+        btn.textContent = isOpen ? "+" : "×";
+    });
 </script>
 @endsection
