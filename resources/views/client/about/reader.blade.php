dd25469b837357d8b35d93701feb6b747ca31001
luatnc <luatnc@efy.com.vn>
2026-02-09 10:21:22 +0700

    f

--- resources/views/client/about/reader.blade.php
+++ resources/views/client/about/reader.blade.php
@@ -58,7 +58,7 @@
                                 @endforeach
                                 @endif
                             </div>
-                            <div class="carousel-inner project-detail__image pswp-gallery" id="gallery">
+                            <div class="carousel-inner project-detail__image pswp-gallery" id="gallery" style="padding: 20px;">
                                 @if(isset($datas->imageBlog) && !empty($datas->imageBlog))
                                 @php $i = 1; @endphp
                                 @foreach($datas->imageBlog as $value)
